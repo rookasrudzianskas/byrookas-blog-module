fdef2dd3ea77a1649da12c77c12e1c7658e10065
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 13:38:26 +0200

    feat: add myTheme
    This is a new theme that I want to use in my app.

--- theme.ts
+++ theme.ts
@@ -4,7 +4,41 @@ const props = {
     "--my-white": "#fff",
     "--my-black": "#1a1a1a",
     "--rokas-brand": "#682BE9",
-    "-my-red": "#db4437",
+    "--my-red": "#db4437",
     "--my-yellow": "#f4b400",
     "--my-green": "#0f9d58",
 }
+
+export const myTheme = buildLegacyTheme({
+    /* Base theme colors */
+    "--black": props ["--my-black"],
+    "--white": props ["-my-white"],
+
+    "--gray": "#666",
+    "--gray-base": "#666",
+
+    "--component-bg": props ["--my-black"],
+    "--component-text-color": props ["--my-white"],
+
+    /* Brand */
+    "--brand-primary": props ["--rokas-brand"],
+
+    // Default button
+    "--default-button-color":"#666",
+    "--default-button-primary-color": props["--rokas-brand"],
+    "--default-button-success-color": props["--my-green"],
+    "--default-button-warning-color": props["--my-yellow"],
+    "--default-button-danger-color": props["--my-red"],
+
+    /* State */
+    "--state-info-color": props ["--rokas-brand"],
+    "--state-success-color": props ["--my-green"],
+    "--state-warning-color": props["--my-yellow"],
+    "--state-danger-color": props["--my-red"],
+
+    /* Navbar */
+    "--main-navigation-color": props["--my-black"],
+    "--main-navigation-color--inverted": props["--my-white"],
+
+    "--focus-color": props["--rokas-brand"],
+});
