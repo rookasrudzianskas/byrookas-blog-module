fa9b573f6fd92f52c11edf02ebc6cb0055dd40a0
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 12:32:51 +0200

    feat: add sanity studio
    This adds the sanity studio to the project.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,11 @@
 import {defineCliConfig} from 'sanity/cli'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
 export default defineCliConfig({
   api: {
-    projectId: '191xzjb7',
-    dataset: 'production'
+    projectId,
+    dataset
   }
 })
