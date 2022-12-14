3889c02d50feff9ba8154e94fdbdb8c6e8773549
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 16:26:28 +0200

    Add categories to blog list
    This commit adds the categories to the blog list.

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 const BlogList = ({posts}: Props) => {
-    // console.log(posts.length)
+    console.log(posts[0].categories);
     return (
         <div>
             <hr className="border-[#F7AB0A] mb-10"/>
@@ -17,7 +17,7 @@ const BlogList = ({posts}: Props) => {
                     <div key={post._id} className="group cursor-pointer flex flex-col">
                         <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
                             <Image src={urlFor(post.mainImage).url()} className="object-cover object-left lg:object-center" alt={post.author.name} fill />
-                            <div>
+                            <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
                                 <div>
                                     <p className="font-bold">{post.title}</p>
                                     <p>
@@ -27,6 +27,20 @@ const BlogList = ({posts}: Props) => {
                                             year: "numeric",
                                         })}
                                     </p>
+                                </div>
+
+                                <div>
+                                    {post.categories.map((categoryData: Category, index) => {
+                                        return (
+                                            <div key={index}>
+                                                {categoryData !== null && (
+                                                    <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                                                        <p>{categoryData.title}</p>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        )
+                                    })}
                                 </div>
                             </div>
                         </div>
