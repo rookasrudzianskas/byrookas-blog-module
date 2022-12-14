2241fc55178e36d6a98a8227f69ff96420d799aa
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 18:46:08 +0200

    feat: add revalidate to post page
    This will revalidate the page every 60 seconds.

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -12,7 +12,7 @@ type Props = {
     };
 };
 
-export const revalidate = 60;
+export const revalidate = 60; // revalidate this page every 60 seconds
 
 export async function generateStaticParams() {
     const query = groq`*[_type=='post']
