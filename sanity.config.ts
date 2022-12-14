8110176212318bdb06e79a4ab666a1437348e70d
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 12:27:42 +0200

    feat: add sanity studio
    This commit adds the sanity studio to the project.
    
    The studio is available at /studio.

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,16 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'rokasrudzianskas blog',
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
-  projectId: '191xzjb7',
-  dataset: 'production',
+export default defineConfig({
+  basePath: "/studio",
+  name: 'ROKAS_RUDZIANSKAS_Content_Studio',
+  title: 'ROKAS RUDZIANSKAS Content Studio',
+
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
