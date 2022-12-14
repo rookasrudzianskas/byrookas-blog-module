8a0ba5237b040e7b287743af5fc8461584ca4c11
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 15:12:43 +0200

    feat: add preview mode
    This commit adds preview mode to the app.

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {previewData} from "next/headers";
+import groq from "groq";
+
+const query = groq`
+    *[_type=='post']{
+    ...,
+    author->,
+    categories[]->,
+    } | order(_createdAt desc)
+`;
 
 const HomePage = ({}) => {
+
+    if(previewData()) {
+        return <div>Preview mode</div>
+    }
+
     return (
         <div>
-
+            <h1>Not in preview mode</h1>
         </div>
     );
 };
