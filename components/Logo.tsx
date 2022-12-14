aee73448c0bf1dde0da8c530bfe3437c345d3e26
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 14:09:43 +0200

    feat: add logo
    - Add logo to the header
    - Add logo to the footer
    - Add logo to the about page

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Image from "next/image";
 
-const Logo = ({}) => {
+const Logo = (props: any) => {
+    const { renderDefault, title } = props;
     return (
-        <div>
-            
+        <div className="flex items-center space-x-2">
+            <Image
+                src="https://yt3.ggpht.com/IQ4OqurVrPmACaf3h5fgTcRInn6QoHz0xN4O5qzhuhY7UKgpDg2A4mGyhWW5vcaGSiVbf_FLdQ=s900-c-k-c0x00ffffff-no-rj"
+                 className="rounded-full ml-3 object-cover"
+                 height={50}
+                 width={50}
+                 alt="Logo"
+            />
+            {renderDefault && <>{renderDefault(props)}</>}
         </div>
     );
 };
--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true
+  },
+  images: {
+    domains: ['images.unsplash.com', 'yt3.ggpht.com']
   }
 }
 
