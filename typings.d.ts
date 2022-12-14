80c62174ab67381b07f340a194f22d9a568dd6d3
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 15:36:59 +0200

    feat: add block type
    This is a new block type that we can use to render content.

--- typings.d.ts
+++ typings.d.ts
@@ -38,3 +38,33 @@ interface Slug {
     _type: "slug";
     current: string;
 }
+
+interface Block {
+    _key: string;
+    _type: "block";
+    children: Span[];
+    markDefs: any[];
+    style: "normal" | "h1" | "h2" | "h3" | "h4" | "blockquote";
+}
+
+interface Span {
+    _key: string;
+    _type: "span";
+    marks: string[];
+    text: string;
+}
+
+interface Category extends Base {
+    description: string;
+    title: string;
+}
+
+interface MainImage {
+    _type: "image";
+    asset: Reference;
+}
+
+interface Title {
+    _type: "string";
+    current: string;
+}
